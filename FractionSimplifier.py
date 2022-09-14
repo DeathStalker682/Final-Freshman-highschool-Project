621a83ebddd27be999b79fbcf85cad04bd0d3e77
DeathMG <109441559+DeathStalker682@users.noreply.github.com>
2022-09-14 15:55:38 -0700

    Update FractionSimplifier.py

--- FractionSimplifier.py
+++ FractionSimplifier.py
@@ -8,22 +8,32 @@
 import math
 
 while True:
-  #fraction input
+
+  print()
+  print("This program will simplify fractions. Type 0 to exit. \n")
+  
+#fraction input
   fraction = input("Please enter a fraction (x/y): ")
   if fraction == "0":
     break
+  while fraction.find("/") < 0:
+    fraction = input("Please enter a fraction (x/y): ")
+    if fraction == "0":
+      exit()
   print()
   
-  #turns fraction into two halves
+#turns fraction into two halves
   numbers = fraction.split("/")
   half1 = int(numbers[0])
   half2 = int(numbers[1])
-  
+
+#get greatest common factor
   gcf = math.gcd(half1,half2)
   
-  
+#if fraction is already in simplest form
   if gcf == 1:
     print(half1,"/",half2," Is the simplest form. ", sep = "")
+#divides fractions by gcf to simplify
   elif gcf != 1:
     numerator1 = half1 // gcf
     denominator1 = half2 // gcf
@@ -32,3 +42,5 @@ while True:
     denominator = round(denominator1, 0)
     
     print(numerator, "/", denominator, " Is the simplest form of ",half1, "/", half2, sep = "")
+    
+#Version 1.1: Made input fraction prompt repeat until a correect input was used. 
